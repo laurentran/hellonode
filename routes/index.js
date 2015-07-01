6a3745bfb7ea64264f96b89ef36a5e77d58649ab
Lauren Tran <latra@microsoft.com>
2015-07-01 10:57:55 -0700

    adding cat page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
+var parseString = require('xml2js').parseString;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET cat page. */
+router.get('/randomcat', function(req, res) {
+    unirest.get("http://thecatapi.com/api/images/get?format=xml&results_per_page=1")
+    .header("api_key", "MTY3ODQ")
+    .header("size", "small")
+    .end(function (result) {
+        console.log(result.body);
+        parseString(result.body, function(err, response) {
+            console.log(response.response.data[0].images[0].image[0].url[0]);
+            var url = response.response.data[0].images[0].image[0].url[0];
+            res.render('randomcat', { title: 'Meow' , image: url  });
+        });
+    });
 });
 
 module.exports = router;
